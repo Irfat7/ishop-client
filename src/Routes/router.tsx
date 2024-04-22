0384b3a8382446892ccd3c8f54cf88f74bb80c95
Irfat <imirfat10@gmail.com>
2024-04-22 16:25:54 +0600

    admin drawer added

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Category from "../pages/Category/Category";
 import ErrorMessage from "../components/ErrorMessage/ErrorMessage";
 import SingleProduct from "../pages/SingleProduct/SingleProduct";
+import Admin from "../Layout/Admin";
 
 const router = createBrowserRouter([
     {
@@ -22,6 +23,40 @@ const router = createBrowserRouter([
                 path: '/product/:productId',
                 element: <SingleProduct />
             }
+        ]
+    },
+    {
+        path: '/admin',
+        element: <Admin></Admin>,
+        children: [
+            {
+                path: '/admin/add-product',
+                element: <p>new product page</p>
+            },
+            {
+                path: '/admin/search',
+                element: <p>search page</p>
+            },
+            {
+                path: '/admin/assign-role',
+                element: <p>assign role page</p>
+            },
+            {
+                path: '/admin/add-category',
+                element: <p>add category page</p>
+            },
+            {
+                path: '/admin/orders',
+                element: <p>orders page</p>
+            },
+            {
+                path: '/admin/sale-events',
+                element: <p>sale events page</p>
+            },
+            {
+                path: '/admin/coupons',
+                element: <p>coupons page</p>
+            },
         ]
     },
     {
