22c427b635f28b470aa38fd3ccae5bd5cc416e61
Irfat <imirfat10@gmail.com>
2024-05-22 20:47:06 +0600

    ongoing event added to the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,14 @@ import CategoryCard from "../../components/nextui/CategoryCard";
 import ProductCard from "../../components/nextui/ProductCard/ProductCard";
 import Loader from "../../components/universe/Loader/Loader";
 import { useGetAllCategory } from "../../hooks/useGetAllCategory";
-import { ICategory } from "../../types";
+import { useGetSaleEvent } from "../../hooks/useGetSaleEvent";
+import { ICategory, IProduct } from "../../types";
 import CarouselHome from "./Carousel/CarouselHome";
 
 const Home = () => {
   const [categories, isCategoryLoading, isCategoryError] = useGetAllCategory();
+  const { event } = useGetSaleEvent()
+
   if (isCategoryError) {
     return <ErrorMessage code={500} message="Internal Server Error" />;
   }
@@ -17,7 +20,7 @@ const Home = () => {
       <CarouselHome />
       <div className="mx-auto md:w-4/6">
         <SectionHeader title="Shop By Category" />
-        <div className="grid grid-cols-2 place-items-center gap-y-4 md:grid-cols-3">
+        <div className="grid grid-cols-2 place-items-center gap-y-4 xl:grid-cols-3">
           {isCategoryLoading && <Loader />}
           {categories.map((category: ICategory) => (
             <CategoryCard
@@ -27,6 +30,17 @@ const Home = () => {
             />
           ))}
         </div>
+        {
+          event &&
+          <>
+            <SectionHeader title={event.name} />
+            <div className="grid gap-5 justify-items-center grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {
+                event.products.map((product: IProduct) => <ProductCard key={product._id} product={product} />)
+              }
+            </div>
+          </>
+        }
         <SectionHeader title="Our Top Selling Products" />
         <div className="grid grid-cols-2 place-items-center gap-y-6 md:grid-cols-3 lg:grid-cols-4">
           {/* <ProductCard />
